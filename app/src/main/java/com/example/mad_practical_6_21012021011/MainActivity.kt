d0bfdcb858c3e641cd3b1b64c32e44a0783b30b5
student <student@gnu.ac.in>
2023-09-15 08:58:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_6_21012021011/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mad_practical_6_21012021011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_practical-6_21012021011"
+include(":app")
